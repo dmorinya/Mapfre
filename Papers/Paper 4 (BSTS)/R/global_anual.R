ea5e3fd87b5b1674a76175a482d023f7fd933231
David Moriña Soler <dmorina@ub.edu>
2023-02-13 14:58:59 +0100

    Update final versions of files related to BMC MRM review.

--- Papers/Paper 4 (BSTS)/R/global_anual.R
+++ Papers/Paper 4 (BSTS)/R/global_anual.R
@@ -8,9 +8,11 @@ library(lubridate)
 library(CausalImpact)
 library(MMWRweek)
 
-mapfre_ts <- read_csv(unzip("Data/MAPFRE_Weekly_data.zip", "MAPFRE_Weekly_data.csv"))
+#mapfre_ts <- read_csv(unzip("Data/MAPFRE_Weekly_data.zip", "MAPFRE_Weekly_data.csv"))
+mapfre_ts <- read_csv("MAPFRE_Weekly_data.csv")
 colnames(mapfre_ts) <- c("Year", "WeekNum", "Code_Real", "Code_Esp", "Code_Group", "Id", "Province", "Sex", "Age", "Acts")
-totals <- read_xlsx("Data/S67_IAL_CUBO_CARTERA_HISTORICA_SALUD_FAMILIAS.xlsx")
+#totals <- read_xlsx("Data/S67_IAL_CUBO_CARTERA_HISTORICA_SALUD_FAMILIAS.xlsx")
+totals <- read_xlsx("S67_IAL_CUBO_CARTERA_HISTORICA_SALUD_FAMILIAS.xlsx")
 colnames(totals) <- c("Date", "No.Fun", "Fun", "Dental", "Reimb", "Ind", "Total")
 totals$Date <- paste0("01/", totals$Date)
 totals$Date <- as.Date(totals$Date, "%d/%m/%Y")
@@ -100,7 +102,43 @@ png("Results/females_2019_2021.png")
 dev.off()
 summary(impact)
 
-### MALES (Sex=2)
+### ONLY MALES (Sex=2)
+males <- mapfre_ts %>% filter(Sex==2) %>% group_by(Year, WeekNum) %>% 
+  summarise(Acts=sum(Acts))
+
+males$Date <- seq(as.Date("2019/1/1"), as.Date("2022/01/07"), "weeks")
+males <- males[-158, ]
+### 2019 vs 2020
+y <- males$Acts[year(males$Date)<=2020 ]
+x <- totals$Total[year(totals$Date)<=2020 ]
+post.period <- c(which(year(males$Date)==2020)[1], which(year(males$Date)==2020)[length(which(year(males$Date)==2020))])
+post.period.response <- y[post.period[1] : post.period[2]]
+y[post.period[1] : post.period[2]] <- NA
+ss <- AddStaticIntercept(list(), y)
+ss <- AddSeasonal(ss, y, nseasons = 52)
+bsts.model <- bsts(y~x, ss, niter = 50000)
+impact <- CausalImpact(bsts.model = bsts.model,
+                       post.period.response = post.period.response)
+png("Results/males_2019_2020.png")
+plot(impact)
+dev.off()
+summary(impact)
+
+### 2019 vs 2021
+y <- males$Acts[year(males$Date)!=2020]
+x <- totals$Total[year(totals$Date)!=2020]
+post.period <- c(54,105)
+post.period.response <- y[post.period[1] : post.period[2]]
+y[post.period[1] : post.period[2]] <- NA
+ss <- AddStaticIntercept(list(), y)
+ss <- AddSeasonal(ss, y, nseasons = 52)
+bsts.model <- bsts(y~x, ss, niter = 50000)
+impact <- CausalImpact(bsts.model = bsts.model,
+                       post.period.response = post.period.response)
+png("Results/males_2019_2021.png")
+plot(impact)
+dev.off()
+summary(impact)
 
 ### OVER 60
 over60 <- mapfre_ts %>% filter(Age=="(59, 200]") %>% group_by(Year, WeekNum) %>% 
@@ -179,5 +217,77 @@ dev.off()
 summary(impact)
 
 ### BARCELONA (Province=08)
+barcelona <- mapfre_ts %>% filter(Province==08) %>% group_by(Year, WeekNum) %>% 
+  summarise(Acts=sum(Acts))
+
+barcelona$Date <- seq(as.Date("2019/1/1"), as.Date("2022/01/07"), "weeks")
+barcelona <- barcelona[-158, ]
+### 2019 vs 2020
+y <- barcelona$Acts[year(barcelona$Date)<=2020 ]
+x <- totals$Total[year(totals$Date)<=2020 ]
+post.period <- c(which(year(barcelona$Date)==2020)[1], which(year(barcelona$Date)==2020)[length(which(year(barcelona$Date)==2020))])
+post.period.response <- y[post.period[1] : post.period[2]]
+y[post.period[1] : post.period[2]] <- NA
+ss <- AddStaticIntercept(list(), y)
+ss <- AddSeasonal(ss, y, nseasons = 52)
+bsts.model <- bsts(y~x, ss, niter = 50000)
+impact <- CausalImpact(bsts.model = bsts.model,
+                       post.period.response = post.period.response)
+png("Results/barcelona_2019_2020.png")
+plot(impact)
+dev.off()
+summary(impact)
+
+### 2019 vs 2021
+y <- barcelona$Acts[year(barcelona$Date)!=2020]
+x <- totals$Total[year(totals$Date)!=2020]
+post.period <- c(54,105)
+post.period.response <- y[post.period[1] : post.period[2]]
+y[post.period[1] : post.period[2]] <- NA
+ss <- AddStaticIntercept(list(), y)
+ss <- AddSeasonal(ss, y, nseasons = 52)
+bsts.model <- bsts(y~x, ss, niter = 50000)
+impact <- CausalImpact(bsts.model = bsts.model,
+                       post.period.response = post.period.response)
+png("Results/barcelona_2019_2021.png")
+plot(impact)
+dev.off()
+summary(impact)
 
 ### VALENCIA (Province=46)
+valencia <- mapfre_ts %>% filter(Province==46) %>% group_by(Year, WeekNum) %>% 
+  summarise(Acts=sum(Acts))
+
+valencia$Date <- seq(as.Date("2019/1/1"), as.Date("2022/01/07"), "weeks")
+valencia <- valencia[-158, ]
+### 2019 vs 2020
+y <- valencia$Acts[year(valencia$Date)<=2020 ]
+x <- totals$Total[year(totals$Date)<=2020 ]
+post.period <- c(which(year(valencia$Date)==2020)[1], which(year(valencia$Date)==2020)[length(which(year(valencia$Date)==2020))])
+post.period.response <- y[post.period[1] : post.period[2]]
+y[post.period[1] : post.period[2]] <- NA
+ss <- AddStaticIntercept(list(), y)
+ss <- AddSeasonal(ss, y, nseasons = 52)
+bsts.model <- bsts(y~x, ss, niter = 50000)
+impact <- CausalImpact(bsts.model = bsts.model,
+                       post.period.response = post.period.response)
+png("Results/valencia_2019_2020.png")
+plot(impact)
+dev.off()
+summary(impact)
+
+### 2019 vs 2021
+y <- valencia$Acts[year(valencia$Date)!=2020]
+x <- totals$Total[year(totals$Date)!=2020]
+post.period <- c(54,105)
+post.period.response <- y[post.period[1] : post.period[2]]
+y[post.period[1] : post.period[2]] <- NA
+ss <- AddStaticIntercept(list(), y)
+ss <- AddSeasonal(ss, y, nseasons = 52)
+bsts.model <- bsts(y~x, ss, niter = 50000)
+impact <- CausalImpact(bsts.model = bsts.model,
+                       post.period.response = post.period.response)
+png("Results/valencia_2019_2021.png")
+plot(impact)
+dev.off()
+summary(impact)
--- Papers/Paper 4 (BSTS)/R/osteopathy_anual.R
+++ Papers/Paper 4 (BSTS)/R/osteopathy_anual.R
@@ -17,26 +17,26 @@ colnames(totals) <- c("Date", "No.Fun", "Fun", "Dental", "Reimb", "Ind", "Total"
 totals$Date <- paste0("01/", totals$Date)
 totals$Date <- as.Date(totals$Date, "%d/%m/%Y")
 totals <- totals[totals$Date >= "2019-01-01" &
-                 totals$Date <= "2022-01-01", ]
+                   totals$Date <= "2022-01-01", ]
 
 df <- approx(x=totals$Date,y=totals$Total, xout=seq(as.Date("2019/1/1"), as.Date("2022/01/07"), "weeks"))
 totals <- data.frame(Date=df$x, Total=df$y)
 
 totals <- totals[-158, ]
 
-oncology <- mapfre_ts[mapfre_ts$Code_Real==33 & mapfre_ts$Code_Esp==33 & mapfre_ts$Code_Group=="10100" & mapfre_ts$Id== 1, ]
+osteopathy <- mapfre_ts[mapfre_ts$Code_Real==54 & mapfre_ts$Code_Esp==54 & mapfre_ts$Code_Group=="10100" & mapfre_ts$Id== 1, ]
 
-oncology <- oncology %>% group_by(Year, WeekNum) %>% summarise(Acts=sum(Acts)) ### Remove duplicates
+osteopathy <- osteopathy %>% group_by(Year, WeekNum) %>% summarise(Acts=sum(Acts)) ### Remove duplicates
 
 ### La darrera setmana de 2021 té uns valors molt baixos en tots els casos, no semblen correctes
-oncology <- oncology[-158, ]
+osteopathy <- osteopathy[-158, ]
 
-oncology$Date <- totals$Date
+osteopathy$Date <- totals$Date
 
 ### 2019 vs 2020
-y <- oncology$Acts[year(oncology$Date)<=2020 ]
+y <- osteopathy$Acts[year(osteopathy$Date)<=2020 ]
 x <- totals$Total[year(totals$Date)<=2020 ]
-post.period <- c(which(year(oncology$Date)==2020)[1], which(year(oncology$Date)==2020)[length(which(year(oncology$Date)==2020))])
+post.period <- c(which(year(osteopathy$Date)==2020)[1], which(year(osteopathy$Date)==2020)[length(which(year(osteopathy$Date)==2020))])
 post.period.response <- y[post.period[1] : post.period[2]]
 y[post.period[1] : post.period[2]] <- NA
 ss <- AddStaticIntercept(list(), y)
@@ -44,13 +44,13 @@ ss <- AddSeasonal(ss, y, nseasons = 52)
 bsts.model <- bsts(y~x, ss, niter = 50000)
 impact <- CausalImpact(bsts.model = bsts.model,
                        post.period.response = post.period.response)
-png("Results/oncology_2019_2020.png")
-  plot(impact)
+png("Results/osteopathy_2019_2020.png")
+plot(impact)
 dev.off()
 summary(impact)
 
 ### 2019 vs 2021
-y <- oncology$Acts[year(oncology$Date)!=2020]
+y <- osteopathy$Acts[year(osteopathy$Date)!=2020]
 x <- totals$Total[year(totals$Date)!=2020]
 post.period <- c(54,105)
 post.period.response <- y[post.period[1] : post.period[2]]
@@ -60,7 +60,7 @@ ss <- AddSeasonal(ss, y, nseasons = 52)
 bsts.model <- bsts(y~x, ss, niter = 50000)
 impact <- CausalImpact(bsts.model = bsts.model,
                        post.period.response = post.period.response)
-png("Results/oncology_2019_2021.png")
-  plot(impact)
+png("Results/osteopathy_2019_2021.png")
+plot(impact)
 dev.off()
 summary(impact)
